c08291d36c94c467f3e37476a9f9fa37b4a94637
daniel <danielkuv@gmail.com>
2019-08-07 13:57:41 +0300

    New logic added

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-import os
 import unittest
 
 
-class Tester(unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_success(self):
-        self.assertTrue(True)
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
 
-    def test_failure(self):
-        self.assertTrue(True)
-        # self.assertTrue(False)
-
-    def tearDown(self):
-        pass
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 
 if __name__ == '__main__':
+    print("Testing main!")
     unittest.main()
