6bf8ff0d4c5ee8c6956349e1745f405ffab2e961
daniel <danielkuv@gmail.com>
2019-08-07 13:37:00 +0300

    Test

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import unittest
 
 class Tester(unittest.TestCase):
 
-      def test_success(self):
+    def setUp(self):
+        print("**************************Testing Travis**************************")
+
+    def test_success(self):
         self.assertTrue(True)
 
-      def test_failure(self):
+    def test_failure(self):
         self.assertTrue(False)
+
+    def tearDown(self):
+        print("**************************Testing Ended**************************")
 
 
 if __name__ == '__main__':
